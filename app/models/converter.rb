297b2b81d134d088b23e5cf67ee7af2fa71dc3dc
Sergey Poustovit <levellevel4@gmail.com>
2015-09-11 17:38:02 +0300

    Validation added

--- app/models/converter.rb
+++ app/models/converter.rb
@@ -1,3 +1,10 @@
+require 'file_size_validator'
 class Converter < ActiveRecord::Base
   mount_uploader :file, FileUploader
+
+  validates :file,
+            :presence => true,
+            :file_size => {
+                :maximum => 1.megabytes.to_i
+            }
 end
